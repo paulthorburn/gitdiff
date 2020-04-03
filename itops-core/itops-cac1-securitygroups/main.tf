74aadc7e0d6dd5a5e222f3a18d4a9027394c5d83
Paul Thorburn <paul.thorburn@o2ebrands.com>
2020-04-03 08:45:30 -0700

    asdf

---
+++ itops-core/itops-cac1-securitygroups/main.tf
@@ -0,0 +1,11 @@
+asdf
+
+
+
+
+        gitdiff=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }})
+        echo Here are the recently modified files
+        echo $gitdiff
+        workspaces=$(echo $gitdiff | rev | cut -d"/" -f2 | rev | uniq)
+        echo Here are the unique workspace folders
+        echo $workspaces
